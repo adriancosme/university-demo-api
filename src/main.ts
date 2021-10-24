d63f3f1744aa2b4f15e5f67728ee617f6372e306
Edwin Cosme <adriancosme15@gmail.com>
2021-10-23 21:51:41 -0500

    log typeorm config

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {SERVER_PORT, TYPEORM_CONFIG} from './config/constants';
+import { SERVER_PORT } from './config/constants';
 import generateTypeormConfigFile from './common/scripts/generate-typeorm-config-file';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(
+    process.env.DATABASE_HOST,
+    process.env.DATABASE_PORT,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD,
+  );
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
   const config = app.get(ConfigService);
-  logger.log(config.get(TYPEORM_CONFIG));
   const port = parseInt(config.get(SERVER_PORT), 10) || 3000;
   generateTypeormConfigFile(config);
   app.setGlobalPrefix('api');
